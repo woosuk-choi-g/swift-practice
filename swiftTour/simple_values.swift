ab2d1b1f6a7abf9b089e9760dc0fbed2404394eb
woosuk.choi <woosuk.choi@influential.co.kr>
2023-08-31 08:48:45 +0000

    summary

--- swiftTour/simple_values.swift
+++ swiftTour/simple_values.swift
@@ -12,3 +12,11 @@ let label = "The width is "
 let width = 94
 let widthLabel = label + String(width)
 print(widthLabel)
+
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+print(appleSummary)
+print(fruitSummary)
