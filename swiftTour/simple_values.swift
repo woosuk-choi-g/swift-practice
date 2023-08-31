06edf6f2fbace320170a4914db27b61d09982c8b
woosuk.choi <woosuk.choi@influential.co.kr>
2023-08-31 08:50:18 +0000

    multiline

--- swiftTour/simple_values.swift
+++ swiftTour/simple_values.swift
@@ -20,3 +20,14 @@ let appleSummary = "I have \(apples) apples."
 let fruitSummary = "I have \(apples + oranges) pieces of fruit."
 print(appleSummary)
 print(fruitSummary)
+
+
+let quotation = """
+    Even though there's whitespace to the left,
+    the actual lines aren't indented.
+        Except for this line.
+    Double quotes ))") can appear without being escaped.
+
+    I still have \(apples + oranges) pieces of fruit.
+    """
+print(quotation)
