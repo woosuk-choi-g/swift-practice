df8df0c8b6bcbdcf80a022dc3db9838d8b940a7a
woosuk.choi <woosuk.choi@influential.co.kr>
2023-09-04 08:00:42 +0000

    properties

--- swiftTour/objectsAndClasses.swift
+++ swiftTour/objectsAndClasses.swift
@@ -44,3 +44,31 @@ class Square: NamedShape {
 let test = Square(sideLength: 5.2, name: "my test square")
 test.area()
 print(test.simpleDescription())
+
+
+class EquilateralTriangle: NamedShape {
+    var sideLength: Double = 0.0
+
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+
+    var perimeter: Double {
+        get {
+            return 3.0 * sideLength
+        }
+        set {
+            sideLength = newValue / 3.0
+        }
+    }
+
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)."
+    }
+}
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter) // 9.3
+triangle.perimeter = 9.9
+print(triangle.sideLength) // 3.3000000000000003
